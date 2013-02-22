1ba989980719fd438505f83486e97da592d4812f
SunJoy <719534493@qq.com>
2013-02-22 11:37:58 +0800

    test
    test

---
+++ SeeyYt/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SeeyYt
+//
+//  Created by SunJoy  on 13-2-22.
+//  Copyright (c) 2013年 SunJoy . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SeeyYt/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  SeeyYt
+//
+//  Created by SunJoy  on 13-2-22.
+//  Copyright (c) 2013年 SunJoy . All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+int num1 = 7;
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+}
+//int(^aBlock) (int) = ^ int num2){
+//    return num1 + num2;
+//}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
